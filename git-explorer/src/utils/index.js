2c8c7bceaa193a341680dd8acb4d94bcfd3d53d0
meharaj.moulasab <meharaj.moulasab@automationanywhere.com>
2021-11-28 19:40:50 +0530

    moved code to utils

--- git-explorer/src/utils/index.js
+++ git-explorer/src/utils/index.js
@@ -6,3 +6,15 @@ export function isValidEmail(string) {
     const validRegex = /^<[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*>$/;
     return validRegex.test(string);
 }
+export function getParsedMessage(message) {
+    let newChild = message.split(' ');
+    return newChild.map(nc => {
+        let str = ` ${nc}`;
+        if (isValidUrl(nc)) {
+            str = <a href={nc}>{` ${nc}`}</a>;
+        } else if(isValidEmail(nc)) {
+            str = <a href={`mailto:${nc}`}>{` ${nc}`}</a>;
+        }
+        return str;
+    });
+}
